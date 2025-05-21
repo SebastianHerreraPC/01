4b5daea708f71e23669689364fa773335aa38081
SebastianHerreraPc <sebehepe00@gmail.com>
2025-05-20 21:01:47 -0600

    peticiones

--- apps/app01.js
+++ apps/app01.js
@@ -6,6 +6,8 @@ import { PopupWithForm } from "./PopupWithForm.js";
 import { UserInfo } from "./userInfo.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+  const token = "be99c1cb-2d4a-4521-b59a-f28ca399a70a";
+
   const editButton = document.querySelector(".profile__edit-button");
   const profileName = document.querySelector(".profile__name-text");
   const inputName = document.querySelector(".popup__input-name");
@@ -24,25 +26,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const userInfo = new UserInfo({
     nameSelector: ".profile__name-text",
     jobSelector: ".profile__job",
+    avatarSelector: ".profile__avatar-img",
   });
 
-  const popupEditCard = new PopupWithForm(".popup__edit", (formData) => {
-    userInfo.setUserInfo({
-      name: formData.name,
-      job: formData.job,
-    });
-    popupEditCard.close();
-  });
+  const popupEditCard = new PopupWithForm(".popup__edit", (formData) => {});
   popupEditCard.setEventListeners();
 
-  const popupAddCard = new PopupWithForm(".popup__add", (formData) => {
-    const { placeName, placeImg } = formData;
-    const newCard = new Cards(placeImg, placeName);
-    const cardElement = newCard.generateCard();
-    cardsSection.addItem(cardElement);
-    popupAddCard.close();
-  });
-
+  const popupAddCard = new PopupWithForm(".popup__add", (formData) => {});
   popupAddCard.setEventListeners();
 
   editButton.addEventListener("click", () => {
@@ -58,37 +48,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
   formEdit.addEventListener("submit", (evt) => {
     evt.preventDefault();
-    profileName.textContent = inputName.value;
-    profileJob.textContent = inputJob.value;
-    popupEditCard.close();
-  });
+    const updatedName = inputName.value;
+    const updatedJob = inputJob.value;
 
-  const initialCards = [
-    {
-      name: "Valle de Yosemite",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-    },
-    {
-      name: "Lago Louise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-    },
-    {
-      name: "Montañas Calvas",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
-    },
-    {
-      name: "Latemar",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-    },
-    {
-      name: "Parque Nacional de la Vanoise",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-    },
-    {
-      name: "Lago di Braies",
-      link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-    },
-  ];
+    fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
+      method: "PATCH",
+      headers: {
+        authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: updatedName,
+        about: updatedJob,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+        return res.json();
+      })
+      .then((userData) => {
+        userInfo.setUserInfo({
+          name: userData.name,
+          job: userData.about,
+          avatar: userData.avatar,
+        });
+        popupEditCard.close();
+      })
+      .catch((err) => {
+        console.error("Error al actualizar perfil:", err);
+      });
+  });
 
   function renderCard(item) {
     const card = new Cards(item.link, item.name);
@@ -98,12 +87,27 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const cardsSection = new Section(
     {
-      items: initialCards,
+      items: [],
       renderer: (item) => renderCard(item),
     },
     cardContainerSelector
   );
-  cardsSection.renderItems();
+
+  fetch("https://around-api.es.tripleten-services.com/v1/cards/", {
+    headers: {
+      authorization: token,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Error: ${res.status}`);
+      return res.json();
+    })
+    .then((cards) => {
+      console.log("Tarjetas cargadas:", cards);
+      cardsSection._items = cards;
+      cardsSection.renderItems();
+    })
+    .catch((err) => console.error("Error al cargar tarjetas:", err));
 
   addButton.addEventListener("click", () => {
     popupAddCard.open();
@@ -123,4 +127,55 @@ document.addEventListener("DOMContentLoaded", function () {
     const validatorAdd = new FormValidator(formAdd);
     validatorAdd.attachValidation();
   }
+
+  fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
+    headers: {
+      authorization: token,
+    },
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error(`Error: ${res.status}`);
+      return res.json();
+    })
+    .then((userData) => {
+      userInfo.setUserInfo({
+        name: userData.name,
+        job: userData.about,
+        avatar: userData.avatar,
+      });
+      console.log("Si toma el usuario", userData);
+    })
+    .catch((err) => console.error("No jala el usuario:", err));
+
+  formAdd.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+
+    const newCardName = placeName.value;
+    const newCardLink = placeImg.value;
+
+    fetch("https://around-api.es.tripleten-services.com/v1/cards/", {
+      method: "POST",
+      headers: {
+        authorization: token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: newCardName,
+        link: newCardLink,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error(`Error: ${res.status}`);
+        return res.json();
+      })
+      .then((newCardData) => {
+        const newCard = new Cards(newCardData.link, newCardData.name);
+        const cardElement = newCard.generateCard();
+        cardsSection.addItem(cardElement);
+        popupAddCard.close();
+      })
+      .catch((err) => {
+        console.error("No se carga la tarjeta", err);
+      });
+  });
 });
